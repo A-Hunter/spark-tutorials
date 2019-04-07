c7051d4ebbba17a44ecd3a927fdd25a1bd4a9f5d
A-Hunter <ghazi.ennacer@gmail.com>
2019-04-07 22:47:45 +0100

    RDD transformations

--- src/main/scala/com/spark/examples/resilient_distributed_datasets/rdd1.scala
+++ src/main/scala/com/spark/examples/resilient_distributed_datasets/rdd1.scala
@@ -31,3 +31,49 @@ dataRDD.foreach(println)
 val words = sc.parallelize(Seq("This", "is", "a", "sequence", "of", "words"))
 val wordPair = words.map(w => (w.charAt(0), w))
 wordPair.foreach(println)
+
+// Map
+val mapFile = dataRDD.map(line => (line, line.length))
+mapFile.foreach(println)
+println(mapFile.count())
+
+// FlatMap
+val flatMapFile = dataRDD.flatMap(line => line.split(" "))
+flatMapFile.foreach(println)
+
+// Union
+val rdd1 = sc.parallelize(Seq((1, "jan", 2016), (3, "nov", 2014), (16, "feb", 2014)))
+val rdd2 = sc.parallelize(Seq((5, "dec", 2014), (17, "sep", 2015), (1, "jan", 2016)))
+val rdd3 = sc.parallelize(Seq((6, "dec", 2016), (16, "may", 2015)))
+
+val rddUnion = rdd1.union(rdd2).union(rdd3)
+rddUnion.foreach(println)
+
+// Intersection
+val rddIntersection = rdd1.intersection(rdd2)
+rddIntersection.foreach(println)
+
+// Distinct
+val rdd4 = sc.parallelize(Seq((6, "dec", 2016), (16, "may", 2015), (6, "dec", 2016)))
+val rddDistinct = rdd4.distinct()
+rddDistinct.foreach(println)
+println(rddDistinct.collect().mkString(","))
+
+// GroupBy
+val rdd5 = sc.parallelize(Array(('k',5),('s',3),('s',4),('p',7),('t',8),('p',5),('k',6)), 3)
+val rddGroup = rdd5.groupByKey().collect()
+rddGroup.foreach(println)
+
+// ReduceBy
+val rddReduce = rdd5.reduceByKey(_+_)
+rddReduce.foreach(println)
+
+// SortBy
+val rddSort = rdd5.sortByKey()
+rddSort.foreach(println)
+
+// Join
+val rdd6 = sc.parallelize(Array(('A',1),('B',2),('C',3)))
+val rdd7 = sc.parallelize(Array(('A',4),('A',6),('B',7),('C',3),('C',8)))
+val rddJoin = rdd6.join(rdd7)
+rddJoin.foreach(println)
